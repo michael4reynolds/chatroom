9d0ff9d394c9055d3b583e55b60f173bbe1606dc
Michael Reynolds <michael4reynolds@gmail.com>
2016-06-20 16:35:40 -0400

    add sockets

--- public/main.js
+++ public/main.js
@@ -1,17 +1,20 @@
 $(() => {
-  console.log('hello')
+  const socket = io()
   const input = $('input')
   const messages = $('#messages')
 
-  const addMessage = (message) => {
+    const addMessage = message => {
     messages.append(`<div>${message}</div>`)
   }
 
-  input.on('keydown', (event) => {
+    input.on('keydown', event => {
     if (event.keyCode != 13) return
 
     const message = input.val()
     addMessage(message)
+    socket.emit('message', message)
     input.val('')
   })
+
+  socket.on('message', addMessage)
 })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
+import socket_io from 'socket.io'
+import http from 'http'
 import express from 'express'
 import * as path from 'path'
-const app = express()
 
+const app = express()
 app.use(express.static(path.join(__dirname, '../public')))
-app.listen(8000)
+
+const server = http.createServer(app)
+const io = socket_io(server)
+
+io.on('connection', socket => {
+  console.log('Client connected')
+
+  socket.on('message', message => {
+    console.log('Received message', message)
+    socket.broadcast.emit('message', message)
+  })
+})
+
+server.listen(8000)
